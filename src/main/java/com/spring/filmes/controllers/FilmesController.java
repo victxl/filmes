6e9bac3e30c34575b56a2a7abe29fa332bbad49f
victxl <victxl@gmail.com>
2023-06-24 10:31:39 -0300

    finalizando

--- src/main/java/com/spring/filmes/controllers/FilmesController.java
+++ src/main/java/com/spring/filmes/controllers/FilmesController.java
@@ -1,26 +1,45 @@
 package com.spring.filmes.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spring.filmes.models.Filme;
+import com.spring.filmes.repository.FilmesRepository;
+
+
 @Controller
 
 
 public class FilmesController {
-
+	@Autowired
+	private FilmesRepository fr;
 
 
     @RequestMapping("/")
     public String index() {
+    	
         return "index";
     }
 
-    @RequestMapping("/cadastrar-filme")
+    @RequestMapping(value ="/cadastrar-filme",method=RequestMethod.GET)
     public String cadastrarFilme() {
         return "cadastrar-filme";
     }
 
-    @RequestMapping("/teste")
-    public String teste() {
-        return "teste";
+    @RequestMapping("/lista-filmes")
+    public String listaFilmes() {
+        return "lista-filmes";
     }
+    
+    @RequestMapping(value="/cadastrar-filme",method=RequestMethod.POST)
+    public String cadastrarFilme(Filme filme) {
+    	fr.save(filme);
+    	return "redirect:/cadastrar-filme";
+    	
+    }
+    
 }
+
+
--- src/main/java/com/spring/filmes/repository/FilmesRepository.java
+++ src/main/java/com/spring/filmes/repository/FilmesRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.spring.filmes.models.Filme;
 
 public interface FilmesRepository extends CrudRepository<Filme, String> {
-
+	Filme findByCodigoFilme(long codigoFilme);
 }
